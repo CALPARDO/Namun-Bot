a91d46c817ad18d92f04b7b076563446f885cb8b
Derin Durak <durakderin@gmail.com>
2020-12-13 18:08:33 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -331,7 +331,8 @@ bot.once("ready", () => {
 
   console.log('Namun is online!');
 
-  bot.user.setActivity('Gossips', { type: 'LISTENING' });
+  //bot.user.setActivity('Gossips', { type: 'LISTENING' });
+  bot.user.setGame('by Listening Gossips', 'https://www.twitch.tv/namun20');
 
   //bot.channels.cache.get('772530698212999209').send("Namun Bot is Online");
 
