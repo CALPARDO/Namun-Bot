61c21c7ec95fad613ecce12bd71a3d72b29b5430
Derin Durak <durakderin@gmail.com>
2021-01-09 13:32:15 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 
 const bot = new Discord.Client();
 
-const TOKEN = 'Nzg2OTEyMTg4NzUzMjQ4MzE3.X9NTFQ.QQay0lrThZcv726yBE1my_nOZzg';
+const TOKEN = 'NAMUN_BOT_TOKEN';
 
 const dict = {
   "c o o l": ["🆒"],
